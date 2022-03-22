b180eaaba4883be26e12a56019c5dea8cd02ba57
Robert Shaw <robertshaw383@gmail.com>
2022-03-22 17:41:32 +0000

    Updated ecp parser

--- pyecpint/__init__.py
+++ pyecpint/__init__.py
@@ -1,1 +1,7 @@
-# empty init
+# initialise package
+def get_ecp_file(name):
+    """Returns where the xml file of the ECP basis with
+       the given name is, for use by ECPBasis.add_ecp_from_file
+    """
+    rootdir = pyecpint.__file__.replace("__init__.py", "data/")
+    return rootdir + name + ".xml"
